3c37e4b2c5d285ccf297821c2ee654f4701529c8
Antonio <antonioandres2000@gmail.com>
2023-03-22 12:08:02 -0400

    commited

--- BurbleSort7.c
+++ BurbleSort7.c
@@ -1,3 +1,4 @@
+
 #include <stdio.h>
 
 void swap(int* xp, int* yp)
@@ -5,18 +6,30 @@ void swap(int* xp, int* yp)
     int temp = *xp;
     *xp = *yp;
     *yp = temp;
+    
 }
 
 // A function to implement bubble sort
 void bubbleSort(int arr[], int n)
 {
+    int count;
     int i, j;
-    for (i = 0; i < n - 1; i++)
-
+    int x = 0;
+   
+    
+    for (i = 0; i < n - 1; i++){
+    count = 0;
+    x++;
         // Last i elements are already in place
         for (j = 0; j < n - i - 1; j++)
-            if (arr[j] > arr[j + 1])
+            if (arr[j] > arr[j + 1]){
                 swap(&arr[j], &arr[j + 1]);
+                count++;
+                
+            }
+    
+    printf("pass #%i: %d\n",x, count);
+    }
 }
 
 /* Function to print an array */
